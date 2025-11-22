bd3da4189b4ebc19e6fad9205008a737c568946f
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-11-22 14:53:27 +0530

    remove elements

---
+++ Remove.js
@@ -0,0 +1,17 @@
+function removeEle(num,val){
+    let x=0;
+    for(let i=0;i<num.length;i++){
+        if(num[i]!= val)
+            {
+            num[i]=num[x];
+            x++;
+
+        }
+    }
+    return x;
+}
+
+let num = [1,2,2,2,3,3,4,4,4,56,65,76,67,3];
+let res=removeEle(num,3)
+
+console.log(res);
