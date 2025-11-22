32c2f7a9858c0ffe39cf1aea1d73ec8e832493bf
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-11-22 13:07:43 +0530

    remove duplicates

--- curring.js
+++ curring.js
@@ -1,16 +1,27 @@
-function findElement(arr,x){
+function findElement(arr){
+    let count=0;
+    let firstLrgest=-Infinity;
+    let secondLargest=-Infinity;
+    if(arr.length<2){
+        return null;
+    }
     for(let i=0;i<arr.length;i++){
-     if (arr[i]==x){
-        return i;
+     if (arr[i]>firstLrgest){
+        secondLargest=firstLrgest;
+        firstLrgest = arr[i];
 
      }
+     else if(arr[i]>secondLargest && arr[i]!= firstLrgest){
+        secondLargest=arr[i];
+     }
+
     
     }
-    return-1;
+    return secondLargest;
 }
 
-let arr=[10,30,40,34,-89];
+let arr=[-89];
 
-let res=findElement(arr,401);
+let res=findElement(arr);
 console.log(res);
 
---
+++ duplicates.js
@@ -0,0 +1,14 @@
+function duplicatesRemove(num){
+    let x=0;
+    for(let i=0;i< num.length;i++){
+        if(num[i]>num[x]){
+            x=x+1;
+            num[x]=num[i];
+        }
+    }
+    return x+1;
+}
+let num=[1,1,2,2,3,4,6,6,7,7,10,10,10,45,344,3452,3452];
+
+let res=duplicatesRemove(num);
+console.log(num);
