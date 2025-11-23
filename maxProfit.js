1de92d6632ed801c9304c0139442858460196821
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-11-23 17:23:58 +0530

    maximum profit

---
+++ maxProfit.js
@@ -0,0 +1,20 @@
+function maxProfit(prices){
+    let min= 0 ;
+    let maxProfit = 0;
+    for (let i =0;i < prices.length;i++)
+        {
+        if (prices[i]-min>maxProfit){
+            maxProfit = prices[i] - min;
+        }
+        if (prices[i] < min) {
+            min = prices[i];
+        }
+    }
+    return maxProfit;
+}
+
+let prices=[6,1,3,5,6,7];
+
+let res = maxProfit(prices);
+
+console.log(res);
