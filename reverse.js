ea026aead00ad4e491265aa8fc15b53a630a1978
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-03 20:20:23 +0530

    recurrsion

--- reverse.js
+++ reverse.js
@@ -1,16 +1,9 @@
- function reverseString(s){
-    let len=s.length;
+var reverseString = function(s) {
+    let len = s.length;
     let halfLen = Math.floor(len/2);
-    for(let i=0;i<halfLen;i++)
-    {
-        let temp = s[i];
-        s[i] = s[len-1-i];
+    for(let i=0;i<halfLen;i++){
+        let temp =s[i];
+        s[i]=s[len-1-i];
         s[len-1-i]=temp;
-    
     }
 };
-
-let s = ["h","e","l","l","o"];
-let res = reverseString(s);
-
-console.log(res);
