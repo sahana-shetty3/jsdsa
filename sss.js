b4f580759473f5d2a5ab7ad0701d8e5568ac90ad
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-05 06:58:26 +0530

    update

--- sss.js
+++ sss.js
@@ -1,1 +1,28 @@
+function findElement(arr){
+    let count=0;
+    let firstLrgest=-Infinity;
+    let secondLargest=-Infinity;
+    if(arr.length<2){
+        return null;
+    }
+    for(let i=0;i<arr.length;i++){
+     if (arr[i]>firstLrgest){
+        secondLargest=firstLrgest;
+        firstLrgest = arr[i];
+
+     }
+     else if(arr[i]>secondLargest && arr[i]!= firstLrgest){
+        secondLargest=arr[i];
+     }
+
+    
+    }
+    return secondLargest;
+}
+
+let arr=[-89];
+
+let res=findElement(arr);
+console.log(res);
+
 
--- star.js
+++ star.js
@@ -1,10 +1,27 @@
-let n=4
-for(let i=0;i<n;i++){
-    row=" ";
-
-
-    for(let j=0;j<n;j++){
-        row=row+" * ";
+function findElement(arr){
+    let count=0;
+    let firstLrgest=-Infinity;
+    let secondLargest=-Infinity;
+    if(arr.length<2){
+        return null;
     }
+    for(let i=0;i<arr.length;i++){
+     if (arr[i]>firstLrgest){
+        secondLargest=firstLrgest;
+        firstLrgest = arr[i];
+
+     }
+     else if(arr[i]>secondLargest && arr[i]!= firstLrgest){
+        secondLargest=arr[i];
+     }
+
+    
+    }
+    return secondLargest;
 }
-console.log(row);
+
+let arr=[-89];
+
+let res=findElement(arr);
+console.log(res);
+
