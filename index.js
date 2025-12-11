bb20acba2a12e86f11faabed9c5d1c27a80d053c
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-11 21:23:11 +0530

    update

--- index.js
+++ index.js
@@ -1,47 +1,7 @@
-// //create arrray
-const numbers=[43,44,56,43,45,34];
- const number2=[1,56,45,3,45,45,4,2,-1,-6];
- console.log(numbers);
- const frt=['apple','banana'];
- console.log(frt);
 
-// const frt1=['apple','banana',22,true,undefined];
-// console.log(frt1);
-
-// let val;
-// val=numbers.length;
-// //check if its array
-// val=Array.isArray(numbers);
-
-
-// //get a single value
-// val=numbers[3];
-// console.log(val);
-
-// //insert or replace
-val=numbers[2]=100;
-
-// //find the index value
- //val=numbers.indexOf(45);
-
-// //mutating array
-
-// //add a numbers to end of the array
-//val=numbers.push(250);
-
-// //add to front of aarray
-//val= numbers.unshift(120);
-
-// //remove the last value from array
-//val=numbers.pop(250);
-
-// //remove the first numbers from array
-//val= numbers.shift();
-
-// //splicing(it will delete numbers)
 //val= numbers.splice(1,2);
 
-// //reverse the array
+
 val= numbers.reverse();
 
 // //concate
--- sss.js
+++ sss.js
@@ -1,28 +1,11 @@
-function findElement(arr){
-    let count=0;
-    let firstLrgest=-Infinity;
-    let secondLargest=-Infinity;
-    if(arr.length<2){
-        return null;
+var removeDuplicates = function(nums) {
+    let x=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[x]){
+            x=x+1;
+            nums[x]=nums[i];
+        }
+        
     }
-    for(let i=0;i<arr.length;i++){
-     if (arr[i]>firstLrgest){
-        secondLargest=firstLrgest;
-        firstLrgest = arr[i];
-
-     }
-     else if(arr[i]>secondLargest && arr[i]!= firstLrgest){
-        secondLargest=arr[i];
-     }
-
-    
-    }
-    return secondLargest;
-}
-
-let arr=[-89];
-
-let res=findElement(arr);
-console.log(res);
-
-
+    return x+1;
+};
