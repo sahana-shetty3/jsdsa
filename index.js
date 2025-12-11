aaaddd8ea087d7b7ae940e71ba605ddd2f30d7c1
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-11 21:24:35 +0530

    update

--- index.js
+++ index.js
@@ -1,28 +1,11 @@
-
-//val= numbers.splice(1,2);
-
-
-val= numbers.reverse();
-
-// //concate
-// val=numbers.concat(number2);
-
-// //sorting
-// val=frt.sort();
-
-// val=numbers.sort();
-
-// val=number2.sort(function(x,y){
-//     y-x;
-// });
-// val=number2.sort(function(x,y){
-//     return x-y;
-// });
-
-
-
-// //get the array length
-console.log(numbers);
- console.log(val);
-
-
+var removeDuplicates = function(nums) {
+    let x=0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>nums[x]){
+            x=x+1;
+            nums[x]=nums[i];
+        }
+        
+    }
+    return x+1;
+};
