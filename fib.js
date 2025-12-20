9110a70a582e73bf67865a37e0252433da467161
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-20 22:12:31 +0530

    update

--- fib.js
+++ fib.js
@@ -3,4 +3,4 @@ var fib = function(n) {
         return n;}
     return fib(n-1)+fib(n-2);
     
-};
+    };
--- sss.js
+++ sss.js
@@ -1,11 +1,10 @@
-var removeDuplicates = function(nums) {
-    let x=0;
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]>nums[x]){
-            x=x+1;
-            nums[x]=nums[i];
+var strStr = function(haystack, needle) {
+    if (needle === "") return 0;
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
         }
-        
     }
-    return x+1;
+    return -1;
 };
