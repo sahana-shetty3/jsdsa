9bfdcd9c8b5e6fbc7baf167bc1b1d7ee76cf28c1
sahana-shetty3 <sahanashetty15103@gmail.com>
2025-12-29 21:25:58 +0530

    update

--- selection.js
+++ selection.js
@@ -1,25 +1,21 @@
-let a=[9,4,8,3,2,1,4,67,45,1,5,6,10]
 
-function selectionSort(a){
-  let n = a.length;
-  for(let i=0;i<n-1;i++){
-    let min = i;
-    for( let j =i+1;j < n ;j++){
-      if( a[j] < a[min]){
-         min = j;
 
-      }
+let res = maxProfit(prices);
+
+console.log(res);
+function maxProfit(prices){
+    let min= 0 ;
+    let maxProfit = 0;
+    for (let i =0;i < prices.length;i++)
+        {
+        if (prices[i]-min>maxProfit){
+            maxProfit = prices[i] - min;
+        }
+        if (prices[i] < min) {
+            min = prices[i];
+        }
     }
-    if( min != i){
-      let temp = a[i];
-      a[i]= a[min];
-      a[min]= temp;
-        
-      }
-  }
-  return a;
-
+    return maxProfit;
 }
 
-let res = selectionSort(a);
-console.log(res);
+let prices=[6,1,3,5,6,7];
