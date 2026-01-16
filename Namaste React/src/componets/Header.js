4f62aadca22bfe43fe9d1c9dc13ae8804932022f
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-01-16 21:31:42 +0530

    intial updates

---
+++ Namaste React/src/componets/Header.js
@@ -0,0 +1,19 @@
+const Header = () =>{
+    return(
+        <div classsName="header">
+            <div className="logo-container">
+                <img className="logo" src=""></img>
+            </div>
+            <div className ="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact US</li>
+                    <li>Cart</li>
+
+                </ul>
+            </div>
+        </div>
+    );
+};
+export default Header;
