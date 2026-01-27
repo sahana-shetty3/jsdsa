4049c0d4c9ae081ec72c9031937b87cdf5c07b24
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-01-27 20:13:21 +0530

    intial updates

---
+++ split.js
@@ -0,0 +1,13 @@
+  var lengthOfLastWord = function (s) {
+      let n = s.length - 1
+      let count = 0
+      while (n >= 0) {
+          if (s[n] != " ") {
+              ++count
+          } else if (count > 0) {
+              break
+          }
+          --n
+      }
+      return count
+  };  
