bcadc6e7f33fda5cddd27c1fa6fde0db3f20a782
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-04 22:22:53 +0530

    updates

--- split.js
+++ split.js
@@ -1,13 +1,20 @@
-  var lengthOfLastWord = function (s) {
-      let n = s.length - 1
-      let count = 0
-      while (n >= 0) {
-          if (s[n] != " ") {
-              ++count
-          } else if (count > 0) {
-              break
-          }
-          --n
-      }
-      return count
-  };  
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    let mapStoT = {};
+    let mapTtoS = {};
+
+    for(let i=0;i < s.length ;i++){
+        if(!mapStoT[s[i]] && !mapTtoS[t[i]]){
+            mapStoT[s[i]] = t[i];
+            mapTtoS[t[i]] = s[i];
+        }
+        else if(mapTtoS[t[i]] !==s[i] || mapStoT[s[i]] !== t[i]){
+            return false;
+        }
+    }
+    return true;
+};
