ad6eb5d50c6c5557c5e2b3256d23dacdcd32af95
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-05 21:53:16 +0530

    updates

--- vowels.js
+++ vowels.js
@@ -25,30 +25,3 @@ var isAnagram = function(s, t) {
 
     
 };
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-var isAnagram = function(s, t) {
-    if(s.length != t.length) return false;
-
-    let map={};
-
-    for(let i=0;i<s.length;i++){
-        (map[s[i]]=!map[s[i]]?1:++map[s[i]])
-    }
-
-    
-    for(i=0;i<t.length;i++){
-        if(!map[t[i]]||map[t[i]]<0){
-            return false;
-        }
-        else{
-            --map[t[i]];
-        }
-    }
-    return true;
-
-    
-};
