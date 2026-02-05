f398fdc5dcc4d1edc08fafd6d400b1077dec4d69
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-05 21:51:39 +0530

    updates

--- vowels.js
+++ vowels.js
@@ -3,18 +3,52 @@
  * @param {string} t
  * @return {boolean}
  */
-var isIsomorphic = function(s, t) {
-    let mapStoT = {};
-    let mapTtoS = {};
+var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
 
-    for(let i=0;i < s.length ;i++){
-        if(!mapStoT[s[i]] && !mapTtoS[t[i]]){
-            mapStoT[s[i]] = t[i];
-            mapTtoS[t[i]] = s[i];
-        }
-        else if(mapTtoS[t[i]] !==s[i] || mapStoT[s[i]] !== t[i]){
+    let map={};
+
+    for(let i=0;i<s.length;i++){
+        (map[s[i]]=!map[s[i]]?1:++map[s[i]])
+    }
+
+    
+    for(i=0;i<t.length;i++){
+        if(!map[t[i]]||map[t[i]]<0){
             return false;
+        }
+        else{
+            --map[t[i]];
         }
     }
     return true;
+
+    
+};
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length != t.length) return false;
+
+    let map={};
+
+    for(let i=0;i<s.length;i++){
+        (map[s[i]]=!map[s[i]]?1:++map[s[i]])
+    }
+
+    
+    for(i=0;i<t.length;i++){
+        if(!map[t[i]]||map[t[i]]<0){
+            return false;
+        }
+        else{
+            --map[t[i]];
+        }
+    }
+    return true;
+
+    
 };
