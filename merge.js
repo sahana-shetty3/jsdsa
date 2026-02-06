124adedc88afb396add6ee6ff63abecda93c62f9
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-06 21:43:36 +0530

    updates

--- merge.js
+++ merge.js
@@ -16,30 +16,3 @@ function con(n) {
 let num = 67977;
 let res = con(num);
 console.log(res);
-/**
- * @param {string} s
- * @param {string} t
- * @return {boolean}
- */
-var isAnagram = function(s, t) {
-    if(s.length != t.length) return false;
-
-    let map={};
-
-    for(let i=0;i<s.length;i++){
-        (map[s[i]]=!map[s[i]]?1:++map[s[i]])
-    }
-
-    
-    for(i=0;i<t.length;i++){
-        if(!map[t[i]]||map[t[i]]<0){
-            return false;
-        }
-        else{
-            --map[t[i]];
-        }
-    }
-    return true;
-
-    
-};
