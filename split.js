bd0e84f7cf2b6fdd252bddfc7abaac4b8aa93b5d
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-02-08 21:49:35 +0530

    updates

--- split.js
+++ split.js
@@ -44,21 +44,3 @@ let res = con(num);
 console.log(res);
 
 
-function con(n) {
-    
-    if (n == 0) {
-        return 1;
-    }
-    n = Math.abs(n);
-    let count = 0;
- 
-    while (n > 0) {
-        n = Math.floor((n/10));
-        count++;
-    }
-    return count;
-
-}
-let num = 67977;
-let res = con(num);
-console.log(res);
