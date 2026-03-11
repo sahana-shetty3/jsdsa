f731e24920b85457ff7a47e0e56c4e588b3d977f
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-03-11 08:17:09 +0530

    updates

--- Remove.js
+++ Remove.js
@@ -1,15 +1,16 @@
-function removeEle(num,val){
+function removeEle(num){
     let x=0;
-    for(let i=0;i<length.num;i++){
+    for(let i=0;i<num.length;i++){
         if(num[i]>num[x]){
-            num[i]=num[x];
-            x++;
+            x=x+1;
+            num[x]=num[i];
         }
     }
-    return x;
+    return x+1;
+    
 }
 
-let num = [1,2,2,2,3,3,4,4,4,56,65,76,67,3];
-let res=removeEle(num,3)
+let num = [1,2,2,2,3,3,4,4,4,56,65];
+let res=removeEle(num)
 
 console.log(res);
