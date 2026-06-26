91c9930e874f701b0fbd0c37a94a6bf8f63dc812
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-06-26 21:54:09 +0530

    EPISODE 1

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+
+
 const parent = React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child1"},
        [ React.createElement("h1",{},"hello this is sahana" ), React.createElement("h2",{},"hiii" )]
