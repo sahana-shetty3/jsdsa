3f623c841aeb78fac85b682412b285aea2f286b2
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-01 20:58:36 +0530

    UPDATED

--- Remove.js
+++ Remove.js
@@ -1,14 +1,63 @@
-var removeDuplicates = function(nums) {
-    let x=0;
+import Header from "./Header.jsx"
+import Body from "./Body.jsx";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./Contact.jsx";
+import Error from "./Error.jsx";
+import RestaurantMenu from "./RestaurantMenu.jsx";
+import { lazy, Suspense } from "react";
 
-    for(let i=0;i<nums.length;i++){
-        if(nums[i] > nums[x]){
-            x++;
-            nums[x]=nums[i];
-        }
-    }
-    return x+1;
+const Grocery = lazy(() => import("./Grocery"));
+const About = lazy(() => import("./About"));
 
-    
-    
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
+
+export default appRouter;
+
+    
+    
+
