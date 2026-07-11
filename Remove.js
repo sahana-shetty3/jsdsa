54838bc175551200d531c03dd4a15fb6b23b5dd1
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-11 20:51:14 +0530

    updated

--- Remove.js
+++ Remove.js
@@ -1,4 +1,15 @@
 
+var removeDuplicates = function(nums) {
+    let x=0;
+
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] > nums[x]){
+            x++;
+            nums[x]=nums[i];
+        }
+    }
+    return x+1;
     
     
 
+}
