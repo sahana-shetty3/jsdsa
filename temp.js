9c7bb4a326c9a4cf90de4248dbe5f0abe6f0948c
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-24 22:21:43 +0530

    updated

--- temp.js
+++ temp.js
@@ -1,26 +1,42 @@
-var dailyTemperatures = function(arr) {
-
-
-    let stack =[];
-
-    let n= arr.length;
-
-    let ans = Array(n).fill(0);
-
-    stack.push(n-1);
-
-    for(let i=n-2;i>=0;i--){
-        while(stack.length){
-            let top = stack[stack.length-1]
-            if(arr[i]>=arr[top]){
-                stack.pop();
-            }
-            else{
-                ans[i]=top-i;
-                break;
+var orangesRotting = function(grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+    let queue = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 2) {
+                queue.push([i, j, 0]);
             }
         }
-        stack.push(i);
     }
-    return ans;
+    let maxMinutes = 0;
+    while (queue.length) {
+        let [x, y, level] = queue.shift();
+
+        if (x > 0 && grid[x - 1][y] === 1) {
+            grid[x - 1][y] = 2;
+            queue.push([x - 1, y, level + 1]);
+        }
+        if (x < m - 1 && grid[x + 1][y] === 1) {
+            grid[x + 1][y] = 2;
+            queue.push([x + 1, y, level + 1]);
+        }
+        if (y < n - 1 && grid[x][y + 1] === 1) {
+            grid[x][y + 1] = 2;
+            queue.push([x, y + 1, level + 1]);
+        }
+        if (y > 0 && grid[x][y - 1] === 1) {
+            grid[x][y - 1] = 2;
+            queue.push([x, y - 1, level + 1]);
+        }
+        maxMinutes = Math.max(level, maxMinutes);
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 1) {
+                return -1;
+            }
+        }
+    }
+    return maxMinutes;
 };
