3aa6f70b4ecedf6c13d95071c9b9de2749071010
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-07-26 16:29:39 +0530

    updating app

--- app.js
+++ app.js
@@ -1,16 +1,63 @@
-import React from "react";
-import ReactDOM from "react-dom/client"
+import Header from "./Header.jsx"
+import Body from "./Body.jsx";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Contact from "./Contact.jsx";
+import Error from "./Error.jsx";
+import RestaurantMenu from "./RestaurantMenu.jsx";
+import { lazy, Suspense } from "react";
+import {Provider} from "react-redux";
+import appStore from "../utils/appStore.jsx";
 
+const Grocery = lazy(() => import("./Grocery"));
+const About = lazy(() => import("./About"));
 
-const parent = React.createElement("div",{id:"parent"},
-    [React.createElement("div",{id:"child1"},
-       [ React.createElement("h1",{},"hello this is sahana" ), React.createElement("h2",{},"hiii" )]
-    )],React.createElement("div",{id:"child2"},
-       [ React.createElement("h1",{},"hello this is sahana" ), React.createElement("h2",{},"hiii" )]
-    )
-)
+const AppLayout = () => {
+  return (
+    <Provider store={appStore}>
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+    </Provider>
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(parent);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <About />
+          </Suspense>
+        ),
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/restaurants/:resId",
+        element: <RestaurantMenu />,
+      },
+      {
+        path: "/grocery",
+        element: (
+          <Suspense fallback={<h1>Loading...</h1>}>
+            <Grocery />
+          </Suspense>
+        ),
+      },
+    ],
+  },
+]);
 
-root.render(parent);
+export default appRouter;
