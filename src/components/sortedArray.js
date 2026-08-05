5a571f7312ea016719c076d45c4f4dea15dcfc69
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-08-05 22:06:42 +0530

    first and last position of array

---
+++ src/components/sortedArray.js
@@ -0,0 +1,31 @@
+var searchRange = function(arr, target) {
+    let l = 0;
+    let r = arr.length - 1;
+    let ans = [-1, -1];
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (arr[m] === target) {
+            ans[0] = m;
+            r = m - 1;
+        } else if (arr[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    l = 0;
+    r = arr.length - 1;
+    while (l <= r) {
+        let m = l + Math.floor((r - l) / 2);
+        if (arr[m] === target) {
+            ans[1] = m;
+            l = m + 1;
+        } else if (arr[m] < target) {
+            l = m + 1;
+        } else {
+            r = m - 1;
+        }
+    }
+    return ans;
+};
+   
