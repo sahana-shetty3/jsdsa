9775e01f2614c17701b6bacde98ffda7a97cdd65
sahana-shetty3 <sahanashetty15103@gmail.com>
2026-08-19 09:11:29 +0530

    Feat.Implementing children routes and outlet(client side routing using Link tags)

---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,14 @@
+const RestaurantMenu =()=>{
+    return(
+        <div className="menu">
+            <h1>Name of Restaurant Name</h1>
+            <h2>Menu</h2>
+            <ul>
+                <li>Biriyani</li>
+                <li>Chiken Kabab</li>
+                <li>Burger</li>
+            </ul>
+        </div>
+    )
+}
+export default RestaurantMenu;
